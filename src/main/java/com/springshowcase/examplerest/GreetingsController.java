4df4b1a1422de47628437e43aff33c7fe64d8aab
Abhishek Kumar <abhishekism0010@gmail.com>
2023-11-17 19:02:51 +0530

    Mapping format change

--- src/main/java/com/springshowcase/examplerest/GreetingsController.java
+++ src/main/java/com/springshowcase/examplerest/GreetingsController.java
@@ -3,15 +3,17 @@ package com.springshowcase.examplerest;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/greetings")
 public class GreetingsController {
     private static final String template = "Hello %s!!";
     private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/greetings")
+    @GetMapping
     public Greetings greetings(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greetings(counter.incrementAndGet(), String.format(template, name));
     }
